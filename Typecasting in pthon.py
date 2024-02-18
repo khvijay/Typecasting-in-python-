c160b1562a5d5e7f4fb2bf092bcd935aa8700b60
Vijay khot <120132133+khvijay@users.noreply.github.com>
2024-02-18 18:52:20 +0530

    Add files via upload

---
+++ Typecasting in pthon.py
@@ -0,0 +1,5 @@
+string = "15"
+number = 7
+string_number = int(string) #throws an error if the string is not a vali integer
+sum = number + int(string)
+print("The sum of both the numbers is: ", sum)
